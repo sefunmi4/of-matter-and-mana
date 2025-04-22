f20df7a798803f1f942fc2fef6a4171a4398425b
sefunmi4 <sefunmiashiru0@gmail.com>
2025-04-22 21:23:25 +0000

    added code sol

--- data structures and algorithms/bit_manipulation.cpp
+++ data structures and algorithms/bit_manipulation.cpp
@@ -60,6 +60,25 @@ class Bit_Manipulation(){
         // input: 
         // output: 	
         // process: l, r swap and move to center or shift left n >> i; check if bit 1 and res & bit << (31-i)
+        uint32_t reverse_bits(uint32_t n) {
+            string binary = "";
+            for (int i = 0; i < 32; i++) {
+                if (n & (1 << i)) {
+                    binary += '1';
+                } else {
+                    binary += '0';
+                }
+            }
+            
+            uint32_t res = 0;
+            for (int i = 0; i < 32; i++) {
+                if (binary[31 - i] == '1') { 
+                    res |= (1 << i);
+                }
+            }
+            
+            return res;
+        }
 
         // 5. missing number	
         // input: 
