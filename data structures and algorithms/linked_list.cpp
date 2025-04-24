8550330f0bd6435f46a1825bdffc771ceeee175d
sefunmi4 <sefunmiashiru0@gmail.com>
2025-04-24 20:05:14 +0000

    and linked list solutions

--- data structures and algorithms/linked_list.cpp
+++ data structures and algorithms/linked_list.cpp
@@ -1,134 +1,97 @@
 #include <vector>
 #include <string>
-#include <unordered_set>
 #include <unordered_map>
-#include <algorithm>
+#include <unordered_set>
+#include <list>
+#include <queue>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// Definition for a linked list node with random pointer.
+struct Node {
+    int val;
+    Node* next;
+    Node* random;
+    Node(int _val) : val(_val), next(nullptr), random(nullptr) {}
+};
+
 class Linked_List {
 public:
-    // Default constructor
     Linked_List() {}
-
-    // Destructor
     ~Linked_List() {}
-
-    // Copy constructor
     Linked_List(const Linked_List& other) = default;
-
-    // Move constructor
     Linked_List(Linked_List&& other) noexcept = default;
-
-    // Copy assignment
     Linked_List& operator=(const Linked_List& other) = default;
-
-    // Move assignment
     Linked_List& operator=(Linked_List&& other) noexcept = default;
 
-    // 1. reverse_linked_list:
-    // Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
-    // /**
-    // * Definition for singly-linked list.
-    // * struct ListNode {
-    // *     int val;
-    // *     ListNode *next;
-    // *     ListNode() : val(0), next(nullptr) {}
-    // *     ListNode(int x) : val(x), next(nullptr) {}
-    // *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-    // * };
-    // */
+    // 1. Reverse Linked List
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = nullptr;
-        ListNode* curr = head;
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
 
+    // 2. Merge Two Sorted Lists
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy;
+        ListNode* current = &dummy;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                current->next = list1;
+                list1 = list1->next;
+            } else {
+                current->next = list2;
+                list2 = list2->next;
+            }
+            current = current->next;
+        }
+        current->next = list1 ? list1 : list2;
+        return dummy.next;
+    }
+
+    // 3. Reorder List
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* prev = nullptr;
+        ListNode* curr = slow->next;
+        slow->next = nullptr;
         while (curr) {
             ListNode* temp = curr->next;
             curr->next = prev;
             prev = curr;
             curr = temp;
         }
-        return prev;
-    }
 
-    // 2.merge_two_sorted_list:
-    // You are given the heads of two sorted linked lists list1 and list2.
-    // Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
-    // The new list should be made up of nodes from list1 and list2.
-    // /**
-    //  * Definition for singly-linked list.
-    //  * struct ListNode {
-    //  *     int val;
-    //  *     ListNode *next;
-    //  *     ListNode() : val(0), next(nullptr) {}
-    //  *     ListNode(int x) : val(x), next(nullptr) {}
-    //  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-    //  * };
-    //  */
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0);
-        ListNode* node = &dummy;
-
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                node->next = list1;
-                list1 = list1->next;
-            } else {
-                node->next = list2;
-                list2 = list2->next;
-            }
-            node = node->next;
-        }
-
-        if (list1) {
-            node->next = list1;
-        } else {
-            node->next = list2;
-        }
-
-        return dummy.next;
-    }
-
-    // 3. reorder_list:
-    // You are given the head of a singly linked-list.
-    // The positions of a linked list of length = 7 for example, can intially be represented as:
-    // [0, 1, 2, 3, 4, 5, 6]
-    // Reorder the nodes of the linked list to be in the following order:
-    // [0, 6, 1, 5, 2, 4, 3]
-    // Notice that in the general case for a list of length = n the nodes are reordered to be in the following order:
-    // [0, n-1, 1, n-2, 2, n-3, ...]
-    // You may not modify the values in the list's nodes, but instead you must reorder the nodes themselves.
-    // /**
-    //  * Definition for singly-linked list.
-    //  * struct ListNode {
-    //  *     int val;
-    //  *     ListNode *next;
-    //  *     ListNode() : val(0), next(nullptr) {}
-    //  *     ListNode(int x) : val(x), next(nullptr) {}
-    //  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-    //  * };
-    //  */
-    void reorderList(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        ListNode* second = slow->next;
-        ListNode* prev = slow->next = nullptr;
-        while (second != nullptr) {
-            ListNode* tmp = second->next;
-            second->next = prev;
-            prev = second;
-            second = tmp;
-        }
-
+        // Merge halves
         ListNode* first = head;
-        second = prev;
-        while (second != nullptr) {
+        ListNode* second = prev;
+        while (second) {
             ListNode* tmp1 = first->next;
             ListNode* tmp2 = second->next;
             first->next = second;
@@ -138,30 +101,14 @@ public:
         }
     }
 
-    // 4. remove_nth_to_last_node:
-    // You are given the beginning of a linked list head, and an integer n.
-    // Remove the nth node from the end of the list and return the beginning of the list.
-    // /**
-    //  * Definition for singly-linked list.
-    //  * struct ListNode {
-    //  *     int val;
-    //  *     ListNode *next;
-    //  *     ListNode() : val(0), next(nullptr) {}
-    //  *     ListNode(int x) : val(x), next(nullptr) {}
-    //  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-    //  * };
-    //  */
+    // 4. Remove Nth Node from End
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* dummy = new ListNode(0, head);
         ListNode* left = dummy;
         ListNode* right = head;
 
-        while (n > 0) {
-            right = right->next;
-            n--;
-        }
-
-        while (right != nullptr) {
+        while (n--) right = right->next;
+        while (right) {
             left = left->next;
             right = right->next;
         }
@@ -170,180 +117,179 @@ public:
         return dummy->next;
     }
 
-    // 5. copy_list_from_random_pointer:
-
-    // Copy Linked List with Random Pointer
-    // You are given the head of a linked list of length n. Unlike a singly linked list, each node contains an additional pointer random, which may point to any node in the list, or null.
-
-    // Create a deep copy of the list.
-
-    // The deep copy should consist of exactly n new nodes, each including:
-
-    // The original value val of the copied node
-    // A next pointer to the new node corresponding to the next pointer of the original node
-    // A random pointer to the new node corresponding to the random pointer of the original node
-    // Note: None of the pointers in the new list should point to nodes in the original list.
-
-    // Return the head of the copied linked list.
-
-    // In the examples, the linked list is represented as a list of n nodes. Each node is represented as a pair of [val, random_index] where random_index is the index of the node (0-indexed) that the random pointer points to, or null if it does not point to any node.
-
-    // /*
-    // // Definition for a Node.
-    // class Node {
-    // public:
-    //     int val;
-    //     Node* next;
-    //     Node* random;
-        
-    //     Node(int _val) {
-    //         val = _val;
-    //         next = NULL;
-    //         random = NULL;
-    //     }
-    // };
-    // */
-
+    // 5. Copy List with Random Pointer
     Node* copyRandomList(Node* head) {
-        if (!head) {
-            return nullptr;
+        if (!head) return nullptr;
+
+        // 1st pass: clone nodes
+        Node* curr = head;
+        while (curr) {
+            Node* copy = new Node(curr->val);
+            copy->next = curr->next;
+            curr->next = copy;
+            curr = copy->next;
         }
 
-        Node* l1 = head;
-        while (l1) {
-            Node* l2 = new Node(l1->val);
-            l2->next = l1->random;
-            l1->random = l2;
-            l1 = l1->next;
-        }
-
-        Node* newHead = head->random;
-
-        l1 = head;
-        while (l1) {
-            Node* l2 = l1->random;
-            l2->random = (l2->next != nullptr) ? l2->next->random : nullptr;
-            l1 = l1->next;
-        }
-
-        l1 = head;
-        while (l1) {
-            Node* l2 = l1->random;
-            l1->random = l2->next;
-            l2->next = (l1->next != nullptr) ? l1->next->random : nullptr;
-            l1 = l1->next;
-        }
-
-        return newHead;
-    }
-
-    // 6. add_two_numbers:
-    // You are given two non-empty linked lists, l1 and l2, where each represents a non-negative integer.
-
-    // The digits are stored in reverse order, e.g. the number 123 is represented as 3 -> 2 -> 1 -> in the linked list.
-
-    // Each of the nodes contains a single digit. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-    // Return the sum of the two numbers as a linked list.
-
-    // /**
-    //  * Definition for singly-linked list.
-    //  * struct ListNode {
-    //  *     int val;
-    //  *     ListNode *next;
-    //  *     ListNode() : val(0), next(nullptr) {}
-    //  *     ListNode(int x) : val(x), next(nullptr) {}
-    //  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-    //  * };
-    //  */
-
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode();
-        ListNode* cur = dummy;
-
-        int carry = 0;
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int v1 = (l1 != nullptr) ? l1->val : 0;
-            int v2 = (l2 != nullptr) ? l2->val : 0;
-
-            int val = v1 + v2 + carry;
-            carry = val / 10;
-            val = val % 10;
-            cur->next = new ListNode(val);
-
-            cur = cur->next;
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
-        }
-        ListNode* res = dummy->next;
-        delete dummy;
-        return res;
-    }
-
-    // 7. linked_list_cycle_detection:
-
-    // Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
-
-    // There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
-
-    // Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
-
-    // Note: index is not given to you as a parameter.
-
-    // /**
-    // * Definition for singly-linked list.
-    // * struct ListNode {
-    // *     int val;
-    // *     ListNode *next;
-    // *     ListNode() : val(0), next(nullptr) {}
-    // *     ListNode(int x) : val(x), next(nullptr) {}
-    // *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-    // * };
-    // */
-
-    bool hasCycle(ListNode* head) {
-        ListNode* fast = head;
-        ListNode* slow = head;
-
-        while (fast != nullptr && fast->next != nullptr) {
-            fast = fast->next->next;
-            slow = slow->next;
-            
-            if (fast == slow) {
-                return true;
+        // 2nd pass: assign random pointers
+        curr = head;
+        while (curr) {
+            if (curr->random) {
+                curr->next->random = curr->random->next;
             }
+            curr = curr->next->next;
         }
-        
+
+        // 3rd pass: restore original & extract copy
+        curr = head;
+        Node* dummy = new Node(0);
+        Node* copyCurr = dummy;
+        while (curr) {
+            Node* copy = curr->next;
+            curr->next = copy->next;
+            copyCurr->next = copy;
+            copyCurr = copy;
+            curr = curr->next;
+        }
+
+        return dummy->next;
+    }
+
+    // 6. Add Two Numbers
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy;
+        ListNode* curr = &dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int v1 = l1 ? l1->val : 0;
+            int v2 = l2 ? l2->val : 0;
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+
+        return dummy.next;
+    }
+
+    // 7. Detect Cycle in Linked List
+    bool hasCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
         return false;
     }
 
-
-    // 8. find_the_duplicate_number:
-    // You are given an array of integers nums containing n + 1 integers. Each integer in nums is in the range [1, n] inclusive.
-    // Every integer appears exactly once, except for one integer which appears two or more times. Return the integer that appears more than once.
+    // 8. Find Duplicate Number
     int findDuplicate(vector<int>& nums) {
         int slow = 0, fast = 0;
-        while (true) {
+        do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-            if (slow == fast) {
-                break;
-            }
-        }
+        } while (slow != fast);
 
         int slow2 = 0;
-        while (true) {
+        while (slow != slow2) {
             slow = nums[slow];
             slow2 = nums[slow2];
-            if (slow == slow2) {
-                return slow;
-            }
         }
+        return slow;
     }
 
+    // 9. LRU Cache
+    class LRUCache {
+    private:
+        unordered_map<int, pair<int, list<int>::iterator>> cache;
+        list<int> order;
+        int capacity;
 
+    public:
+        LRUCache(int capacity) : capacity(capacity) {}
 
+        int get(int key) {
+            if (cache.find(key) == cache.end()) return -1;
+            order.erase(cache[key].second);
+            order.push_back(key);
+            cache[key].second = prev(order.end());
+            return cache[key].first;
+        }
+
+        void put(int key, int value) {
+            if (cache.find(key) != cache.end()) {
+                order.erase(cache[key].second);
+            } else if (cache.size() == capacity) {
+                int lru = order.front();
+                order.pop_front();
+                cache.erase(lru);
+            }
+            order.push_back(key);
+            cache[key] = {value, prev(order.end())};
+        }
+    };
+
+    // 10. Merge K Sorted Lists
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
+
+        for (auto list : lists) {
+            if (list) pq.push(list);
+        }
+
+        ListNode dummy;
+        ListNode* curr = &dummy;
+
+        while (!pq.empty()) {
+            ListNode* node = pq.top(); pq.pop();
+            curr->next = node;
+            curr = curr->next;
+            if (node->next) pq.push(node->next);
+        }
+
+        return dummy.next;
+    }
+
+    // 11. Reverse Nodes in k-Group
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* groupPrev = dummy;
+
+        while (true) {
+            ListNode* kth = getKth(groupPrev, k);
+            if (!kth) break;
+
+            ListNode* groupNext = kth->next;
+            ListNode* prev = kth->next;
+            ListNode* curr = groupPrev->next;
+
+            while (curr != groupNext) {
+                ListNode* tmp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = tmp;
+            }
+
+            ListNode* tmp = groupPrev->next;
+            groupPrev->next = kth;
+            groupPrev = tmp;
+        }
+
+        return dummy->next;
+    }
 
 private:
-    // no private variables currently
+    ListNode* getKth(ListNode* curr, int k) {
+        while (curr && k > 0) {
+            curr = curr->next;
+            k--;
+        }
+        return curr;
+    }
 };
