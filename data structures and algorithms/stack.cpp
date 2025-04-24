37d1817b1360cace08bca0fe93e2008a261b92e8
sefunmi4 <sefunmiashiru0@gmail.com>
2025-04-24 01:21:59 +0000

    cleaned up file

--- data structures and algorithms/stack.cpp
+++ data structures and algorithms/stack.cpp
@@ -1,246 +1,152 @@
 #include <vector>
 #include <string>
+#include <stack>
 #include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
-class Stack{
+class Stack {
 public:
-    // Default constructor
     Stack() {}
-
-    // Destructor
     ~Stack() {}
-
-    // Copy constructor
     Stack(const Stack& other) = default;
-
-    // Move constructor
     Stack(Stack&& other) noexcept = default;
-
-    // Copy assignment
     Stack& operator=(const Stack& other) = default;
-
-    // Move assignment
     Stack& operator=(Stack&& other) noexcept = default;
 
-    // 1. valid_parentheses:
-    // You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
-    // The input string s is valid if and only if:
-    // Every open bracket is closed by the same type of close bracket.
-    // Open brackets are closed in the correct order.
-    // Every close bracket has a corresponding open bracket of the same type.
-    // Return true if s is a valid string, and false otherwise.
-    // stack.append(item) if open_[ else (pop if close_item match) else invalid
-    // I want to use this solution: stack.append(item) if open_[ else (pop if close_item match) else invalid
+    // 1. Valid Parentheses
     bool isValid(string s) {
-        while (true) {
-            size_t pos = string::npos;
-            if ((pos = s.find("()")) != string::npos) {
-                s.erase(pos, 2);
-                continue;
-            }
-            if ((pos = s.find("{}")) != string::npos) {
-                s.erase(pos, 2);
-                continue;
-            }
-            if ((pos = s.find("[]")) != string::npos) {
-                s.erase(pos, 2);
-                continue;
-            }
-            break;
-        }
-        return s.empty();
-    }
-
-    // 2. min stack:
-    // Design a stack class that supports the push, pop, top, and getMin operations.
-
-    // MinStack() initializes the stack object.
-    // void push(int val) pushes the element val onto the stack.
-    // void pop() removes the element on the top of the stack.
-    // int top() gets the top element of the stack.
-    // int getMin() retrieves the minimum element in the stack.
-    // Each function should run in O(1) time.
-    class MinStack {
-    public:
-        stack<int> stk;
-        MinStack() {
-            
-        }
-        
-        void push(int val) {
-            stk.push(val);
-        }
-        
-        void pop() {
-            stk.pop();
-        }
-        
-        int top() {
-            return stk.top();
-        }
-        
-        int getMin() {
-            stack<int> tmp;
-            int mini = stk.top();
-            while (stk.size()) {
-                mini = min(mini, stk.top());
-                tmp.push(stk.top());
+        stack<char> stk;
+        unordered_map<char, char> match = {{')','('}, {']','['}, {'}','{'}};
+        for (char c : s) {
+            if (match.count(c)) {
+                if (stk.empty() || stk.top() != match[c]) return false;
                 stk.pop();
-            }
-
-            while (tmp.size()) {
-                stk.push(tmp.top());
-                tmp.pop();
-            }
-
-            return mini;
-        }
-    };
-
-    // 3. evaluate reverse polish notation:
-    // You are given an array of strings tokens that represents a valid arithmetic expression in Reverse Polish Notation.
-    // Return the integer that represents the evaluation of the expression.
-    // The operands may be integers or the results of other operations.
-    // The operators include '+', '-', '*', and '/'.
-    // Assume that division between integers always truncates toward zero.
-    int evalRPN(vector<string>& tokens) {
-        while (tokens.size() > 1) {
-            for (int i = 0; i < tokens.size(); i++) {
-                if (tokens[i] == "+" 
-                    || tokens[i] == "-" 
-                    || tokens[i] == "*" 
-                    || tokens[i] == "/") 
-                {
-                    int a = stoi(tokens[i - 2]);
-                    int b = stoi(tokens[i - 1]);
-                    int result = 0;
-                    if (tokens[i] == "+") result = a + b;
-                    else if (tokens[i] == "-") result = a - b;
-                    else if (tokens[i] == "*") result = a * b;
-                    else if (tokens[i] == "/") result = a / b;
-                    
-                    tokens.erase(tokens.begin() + i - 2, tokens.begin() + i + 1);
-                    tokens.insert(tokens.begin() + i - 2, to_string(result));
-                    break;
-                }
+            } else {
+                stk.push(c);
             }
         }
-        return stoi(tokens[0]);
+        return stk.empty();
     }
 
-    // 4. generate parantheses:
-    // You are given an integer n. Return all well-formed parentheses strings that you can generate with n pairs of parentheses.
-    class Solution {
+    // 2. Min Stack
+    class MinStack {
+    private:
+        stack<int> mainStack;
+        stack<int> minStack;
     public:
-        bool valid(const string& s) {
-            int open = 0;
-            for (char c : s) {
-                open += (c == '(') ? 1 : -1;
-                if (open < 0) return false;
+        MinStack() {}
+
+        void push(int val) {
+            mainStack.push(val);
+            if (minStack.empty() || val <= minStack.top()) {
+                minStack.push(val);
             }
-            return open == 0;
         }
 
-        void dfs(string s, vector<string>& res, int n) {
-            if (s.length() == 2 * n) {
-                if (valid(s)) res.push_back(s);
-                return;
+        void pop() {
+            if (mainStack.top() == minStack.top()) {
+                minStack.pop();
             }
-            dfs(s + '(', res, n);
-            dfs(s + ')', res, n);
+            mainStack.pop();
         }
 
-        vector<string> generateParenthesis(int n) {
-            vector<string> res;
-            dfs("", res, n);
-            return res;
+        int top() {
+            return mainStack.top();
+        }
+
+        int getMin() {
+            return minStack.top();
         }
     };
 
-    // 5. daily temperatures:
-    // You are given an array of integers temperatures where temperatures[i] represents the daily temperatures on the ith day.
-    // Return an array result where result[i] is the number of days after the ith day before a warmer temperature appears on a future day. If there is no day in the future where a warmer temperature will appear for the ith day, set result[i] to 0 instead.
+    // 3. Evaluate Reverse Polish Notation
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+        for (const string& token : tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int b = stk.top(); stk.pop();
+                int a = stk.top(); stk.pop();
+                if (token == "+") stk.push(a + b);
+                else if (token == "-") stk.push(a - b);
+                else if (token == "*") stk.push(a * b);
+                else if (token == "/") stk.push(a / b);
+            } else {
+                stk.push(stoi(token));
+            }
+        }
+        return stk.top();
+    }
+
+    // 4. Generate Parentheses
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        generateParenthesisDFS("", 0, 0, n, res);
+        return res;
+    }
+
+    void generateParenthesisDFS(string s, int open, int close, int n, vector<string>& res) {
+        if (s.length() == 2 * n) {
+            res.push_back(s);
+            return;
+        }
+        if (open < n) generateParenthesisDFS(s + "(", open + 1, close, n, res);
+        if (close < open) generateParenthesisDFS(s + ")", open, close + 1, n, res);
+    }
+
+    // 5. Daily Temperatures (Optimized using monotonic stack)
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
-        vector<int> res(n);
+        vector<int> res(n, 0);
+        stack<int> stk;
 
-        for (int i = 0; i < n; i++) {
-            int count = 1;
-            int j = i + 1;
-            while (j < n) {
-                if (temperatures[j] > temperatures[i]) {
-                    break;
-                }
-                j++;
-                count++;
+        for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && temperatures[i] > temperatures[stk.top()]) {
+                int idx = stk.top(); stk.pop();
+                res[idx] = i - idx;
             }
-            count = (j == n) ? 0 : count;
-            res[i] = count;
+            stk.push(i);
         }
         return res;
     }
 
-    // 6. car fleet:
-    // There are n cars traveling to the same destination on a one-lane highway.
-    // You are given two arrays of integers position and speed, both of length n.
-    // position[i] is the position of the ith car (in miles)
-    // speed[i] is the speed of the ith car (in miles per hour)
-    // The destination is at position target miles.
-    // A car can not pass another car ahead of it. It can only catch up to another car and then drive at the same speed as the car ahead of it.
-    // A car fleet is a non-empty set of cars driving at the same position and same speed. A single car is also considered a car fleet.
-    // If a car catches up to a car fleet the moment the fleet reaches the destination, then the car is considered to be part of the fleet.
-    // Return the number of different car fleets that will arrive at the destination.
+    // 6. Car Fleet
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int, int>> pair;
+        vector<pair<int, int>> car_data;
         for (int i = 0; i < position.size(); i++) {
-            pair.push_back({position[i], speed[i]});
+            car_data.push_back({position[i], speed[i]});
         }
-        sort(pair.rbegin(), pair.rend());
-        vector<double> stack;
-        for (auto& p : pair) {
-            stack.push_back((double)(target - p.first) / p.second);
-            if (stack.size() >= 2 && 
-                stack.back() <= stack[stack.size() - 2]) 
-            {
-                stack.pop_back();
+        sort(car_data.rbegin(), car_data.rend());
+
+        vector<double> fleets;
+        for (auto& [pos, spd] : car_data) {
+            double time = (double)(target - pos) / spd;
+            if (fleets.empty() || time > fleets.back()) {
+                fleets.push_back(time);
             }
         }
-        return stack.size();
+        return fleets.size();
     }
 
-    // largest rectangle in histogram:
-    // You are given an array of integers heights where heights[i] represents the height of a bar. The width of each bar is 1.
-    // Return the area of the largest rectangle that can be formed among the bars.
-    // Note: This chart is known as a histogram.
+    // 7. Largest Rectangle in Histogram
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
+        stack<int> stk;
+        heights.push_back(0); // Sentinel
         int maxArea = 0;
 
-        for (int i = 0; i < n; i++) {
-            int height = heights[i];
-
-            int rightMost = i + 1;
-            while (rightMost < n && heights[rightMost] >= height) {
-                rightMost++;
+        for (int i = 0; i < heights.size(); ++i) {
+            while (!stk.empty() && heights[i] < heights[stk.top()]) {
+                int h = heights[stk.top()]; stk.pop();
+                int w = stk.empty() ? i : i - stk.top() - 1;
+                maxArea = max(maxArea, h * w);
             }
-
-            int leftMost = i;
-            while (leftMost >= 0 && heights[leftMost] >= height) {
-                leftMost--;
-            }
-
-            rightMost--;
-            leftMost++;
-            maxArea = max(maxArea, height * (rightMost - leftMost + 1));
+            stk.push(i);
         }
         return maxArea;
     }
 
 private:
-    // no private variables currently
+    // No internal members yet
 };
